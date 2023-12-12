a615fdc263fcfc2d69a985d70d5029796e07812a
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-12-13 02:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// Car constructor
-function Car(make, model) {
-  this.make = make;
-  this.model = model;
+//car constructor
+function Car(make,model){
+	this.make=make;
+	this.model=model;
 }
-
-// Method added to Car prototype
-Car.prototype.getMakeModel = function () {
-  return `${this.make} ${this.model}`;
+Car.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model}`;
 };
 
-// SportsCar constructor
-function SportsCar(make, model, topSpeed) {
-  Car.call(this, make, model);
-  this.topSpeed = topSpeed;
+// SportsCar Constructor
+function SportsCar(make,model,topSpeed){
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
 }
 
-// Inherit Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
 
-// Set SportsCar constructor
-SportsCar.prototype.constructor = SportsCar;
 
-// Method added to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-  return this.topSpeed;
-};
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
 
-window.Car = Car;
-window.SportsCar = SportsCar;
+window.Car=Car;
+window.SportsCar=SportsCar;
+
+
+
+
+
+
+
+
